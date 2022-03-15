d8e88447ef22ae0496dc8ceb32b26ed92454d901
Liam Ok <liamokane19@gmail.com>
2022-03-15 16:00:54 +1100

    updated the associations because it wasn't pulling in the correct relationships. Updated the api route for children so it shows the task page based on the child id and it's pulling in the information of the task their assigned

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Child, Chores, Parent } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -18,6 +18,51 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+router.get('/tasks/child/:id', async (req, res) => {
+ 
+  // this route is meant to be visited by a child, not a parent!
+  // validate
+  // 1. make sure user is logged in
+
+  // 2. check user is a parent or a child
+  // 2. [deprecated] make sure parent can only view their own children
+  // if a parent visits this page, redirect the parent to the task managemnt page
+
+  // 3. if a child is visiting, validate req.param.id === req.user.id
+
+  // if(req.params.id !== req.user.id){
+  //   return res.status(301).redirect('/');
+  // }
+
+  
+  // grab the parent info
+  // grab the child info
+  // get all the relevant chors
+  const child = (await Child.findByPk(req.params.id, {
+    include: [
+      {
+        model: Parent
+      },
+      {
+        model: Chores,
+        
+      }
+    ]
+  })).toJSON();
+
+
+  
+
+
+  // load them to handlebars
+  res.render('childTaskpage', {
+    abc: 123,
+    child: child,
+    parent: child.parent,
+    chores: child.Chores,
+  })
+
 });
 
 router.get('/login', (req, res) => {
--- models/index.js
+++ models/index.js
@@ -6,47 +6,40 @@ const Chores = require('./Chores');
 const Parent = require('./Parent');
 
 
-// Parent.hasMany(Child, {
-//     // foreignKey: 'child_id',
-//     onDelete: 'CASCADE',
-// });
+Parent.hasMany(Child, {
+    // foreignKey: 'child_id',
+    onDelete: 'CASCADE',
+});
 
-// Child.belongsTo(Parent, {
-//     foreignKey: 'parent_id',
-//     onDelete: 'CASCADE',
-// });
+Child.belongsTo(Parent, {
+    foreignKey: 'parent_id',
+    onDelete: 'CASCADE',
+});
 
-// Parent.hasMany(Chores, {
-//     foreignKey: 'chores_id',
-//     onDelete: 'CASCADE',
-// });
 
-// Chores.belongsTo(Parent, {
-//     foreignKey: 'parent_id',
-// });
 
-// Child.hasMany(Chores, {
-//     foreignKey: 'chores_id',
-//     onDelete: 'CASCADE',
-// });
+Child.hasMany(Chores, {
+    
+    onDelete: 'CASCADE',
+});
 
-// Chores.belongsTo(Child, {
-//     foreignKey:'child_id',
-// });
+Chores.belongsTo(Child, {
+    foreignKey:'child_id',
+});
 
-// User.hasOne(Parent, {
-//     foreginKey: 'parent_id', 
-//     onDelete: 'CASCADE',
-// }); 
+User.hasOne(Parent, {
+    foreginKey: 'parent_id', 
+    onDelete: 'CASCADE',
+}); 
 
 Parent.belongsTo(User, {
     foreignKey: 'user_id', 
 });
 
-// User.hasOne(Child, {
-//     foreginKey: 'user_id', 
-//     onDelete: 'CASCADE',
-// }); 
+User.hasOne(Child, {
+    foreginKey: 'user_id', 
+    onDelete: 'CASCADE',
+}); 
 
 Child.belongsTo(User, {
     foreignKey: 'user_id', 
