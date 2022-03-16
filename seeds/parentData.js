0661ba97d9dc9c9f35a61e579b0f303da6b58a0e
Sharon <fubzee@gmail.com>
2022-03-16 16:42:57 +1100

    fixed loging id retrieval issue

--- seeds/parentData.js
+++ seeds/parentData.js
@@ -5,29 +5,48 @@ const parentdata = [
     name: 'Joanne',
     email: 'jo@mail.com',
     chart: 'Bowen Family Chart',
-    user_id: '1',
   },
   {
     name: 'Sommer',
     email: 'Sommer@mail.com',
     chart: 'Our daily Chores',
-    user_id: '2',
+  
   },
   {
     name: 'Harmoney',
     email: 'peace@mail.com',
     chart: 'Our To Do List',
-    user_id: '5',
+    
   },
   {
     name: 'Les',
     email: 'Les@mail.com',
     chart: 'My Reward Tracker',
-    user_id: '7',
+    
   },
 ];
 
-const seedParent = () => Parent.bulkCreate(parentdata);
+const seedParent = () => {
+  
+Parent.bulkCreate(parentdata);
+
+// console.log(seedParent);
+// for (const { id } of parentdata) {
+//     const userid = async () => await User.findOne({
+//     where: {
+//     username : email
+//     }});   
+//     console.log ("found match",userid.value); 
+//     if (userid) {
+//       const updid = Parent.update({
+//         user_id: userid,
+//         where: {
+//           id: userid}
+//         })
+//     }
+
+//   }
+}
 
 module.exports = seedParent;
 
