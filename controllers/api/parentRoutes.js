34201d83e2e01ee2690bb5497a6ddaaad11a95ce
Liam Ok <liamokane19@gmail.com>
2022-03-20 20:07:02 +1100

    updated parent route

--- controllers/api/parentRoutes.js
+++ controllers/api/parentRoutes.js
@@ -44,7 +44,7 @@ router.get('/user/:id',  async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-
+console.log(req.body)
     try {
       const parentData = await Parent.create({
         name: req.body.name,
